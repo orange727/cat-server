ffc9234ca6c844be91a11f916721c6b95382c847
yanweiqing <yanweiqing@meituan.com>
2015-10-15 12:40:57 +0800

    readme规则优化

--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ var  React        = require('react');
 var  koa          = require('koa');
 var  koarouter    = require('koa-router');
 var  koastatic    = require('koa-static');
+var  marked       = require('marked');
 
 var app = koa(),
     router = koarouter();
@@ -13,33 +14,41 @@ var __root = function (dir) { return path.join(path.dirname(__dirname), dir); }
 
 
 var files = fs.readdirSync('examples'),
-    navList = files.map(function (filename) {
-    return `/examples/${filename.split('.')[0]}`;
-});
+    jsxReg = new RegExp('.jsx$'),
+    navList = files.filter(function (filename) {
+        return jsxReg.test(filename);
+    }).map(function (filename) {
+        return `/examples/${filename.split('.')[0]}`;
+    });
 navList.unshift('/readme');
 
 // var layout = fs.readFileSync(__root('layout.html'));
 
 //readme
 router.get('/readme', function *(next) {
-    var layout = fs.readFileSync(__root('layout.html'));
-    var readme = fs.readFileSync('readme.md', 'utf8'),
+    var layout = fs.readFileSync(__root('layout.html')),
+        readme = fs.readFileSync('readme.md', 'utf8'),
         data = {
             navList: navList,
-            body: readme,
-            script: ''
+            script: '',
+            readme: marked(readme),
+            code: ''
         };
     this.body = _.template(layout)(data);
 });
 
 //examples
 router.get('/examples/:example', function *(next) {
-    var layout = fs.readFileSync(__root('layout.html'));
-    var data = {
-        navList: navList,
-        body: fs.readFileSync(`${process.cwd()}/examples/${this.params.example}.jsx`, 'utf8'),
-        script: this.params.example
-    };
+    var layout = fs.readFileSync(__root('layout.html')),
+        readmeFileName = `${process.cwd()}/examples/${this.params.example}.md`,
+        readme = fs.existsSync(readmeFileName) ? fs.readFileSync(readmeFileName, 'utf8') : '',
+        data = {
+            navList: navList,
+            script: this.params.example,
+            readme: marked(readme),
+            code: fs.readFileSync(`${process.cwd()}/examples/${this.params.example}.jsx`, 'utf8')
+        };
+
     this.body =  _.template(layout)(data);
     //前端渲染而非同构
     // var Example = require(`${process.cwd()}/examples/${this.params.example}.jsx`);
