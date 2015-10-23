b6cb147a1107bdaa9b7434d7dc9b9e989a13209c
李泉水 <liquanshui@meituan.com>
2015-10-23 12:13:51 +0800

    添加了less文件编译,

--- server/build-es5.js
+++ server/build-es5.js
@@ -1,19 +1,24 @@
 var path = require('path');
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var less = require('gulp-less');
 var runSequence = require('run-sequence');
 var rename = require("gulp-rename");
 
 var cwd = process.cwd(),
     paths = {
         script: [path.join(cwd, '/src/**/*.jsx'), path.join(cwd, '/src/**/*.js')],
-        scriptDest: path.join(cwd, '/dist/')
+        dest: path.join(cwd, '/dist/'),
+        css: [path.join(cwd, '/assets/**/*.less')]
     };
 
 gulp.task('build-es5', function () {
     gulp.src(paths.script)
-        .pipe(babel())
-        .pipe(gulp.dest(paths.scriptDest));
+        .pipe(babel({stage:0}))
+        .pipe(gulp.dest(paths.dest));
+    gulp.src(paths.css)
+        .pipe(less())
+        .pipe(gulp.dest(paths.dest));
 });
 
 
